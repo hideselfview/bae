fb59ab9639c003109ab5c2711bde8e1383f2c0a8
Dmitry Minkovsky <dminkovsky@gmail.com>
2025-11-12 16:17:54 -0500

    Remove allow(dead_code) and only export what's actually used

--- bae/src/cd/mod.rs
+++ bae/src/cd/mod.rs
@@ -11,12 +11,6 @@ pub mod paranoia;
 pub mod ripper;
 
 pub use cue_generator::CueGenerator;
-#[allow(dead_code)] // Public API exports for external use
-pub use drive::{CdDrive, CdDriveError, CdToc};
-#[allow(dead_code)] // Public API exports for external use
-pub use ffi::{detect_drives, LibcdioDrive, LibcdioError};
+pub use drive::CdDrive;
 pub use log_generator::LogGenerator;
-#[allow(dead_code)] // Public API exports for external use
-pub use paranoia::{ParanoiaError, ParanoiaReader};
-#[allow(dead_code)] // Public API exports for external use
-pub use ripper::{CdRipper, RipProgress, RipResult};
+pub use ripper::{CdRipper, RipProgress};
